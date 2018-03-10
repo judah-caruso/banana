c007c26af91736d098dfdb104ff5d6bc0564085f
Judah <judahrcr@gmail.com>
2018-03-10 01:38:00 -0600

    0.2.4

--- README.md
+++ README.md
@@ -1,3 +1,103 @@
 # Banana.js
 
 A banana library to end all banana libraries.
+
+[![Maintainability](https://api.codeclimate.com/v1/badges/a7b2a7e8b93b120f0025/maintainability)](https://codeclimate.com/github/kyoto-shift/banana/maintainability) [![Test Coverage](https://api.codeclimate.com/v1/badges/a7b2a7e8b93b120f0025/test_coverage)](https://codeclimate.com/github/kyoto-shift/banana/test_coverage)
+
+## Set up 
+
+**Installing**
+```
+npm install banana-js
+```
+
+**Importing**
+
+``Module:``
+```javascript
+var banana = require("banana-js");
+```
+
+``Browser:``
+```html
+<script type="text/javascript" src="./banana.js"></script>
+```
+
+## Usage
+
+Banana currently supports 34 languages, including:
+> Arabic, Azerbaijani, Basque, Belarusian, Burmese, Catalan, Cebuano, Cantonese (and Mandarin), Czech, Dutch, Esperanto, English, Estonian, Filipino, Finnish, French, Georgian, German, Greek, Gujarati, Haitian Creol, Hausa, Hebrew, Hindi, Hungarian, Icelandic, Igbo, Indonesian, Japanese, Kannada, Kazakh, Khmer, and Korean!
+
+
+```javascript
+banana("arabic"); 	// موز
+banana("english"); 	// banana
+banana("korean");	// 바나나
+```
+
+Ever needed the ingredients or directions to make banana nut muffins, pudding, or even smoothies?
+
+```javascript
+// banana.muffin("ingredients" | "directions");
+// banana.pudding("ingredients" | "directions");
+
+banana.smoothie("ingredients" | "directions");
+// ingredients:
+	// [ "1 banana", "1/2 to 1 cup chilled milk", 
+	// "1 tablespoon honey (optional)", 
+	// "5 to 8 ice cubs (optional)" ]
+// directions:
+	// [ "0. Combine all of the ingredients in a blender and blend on medium-high speed until desired thickness.", 
+	// "Tip: Combine ice cubes and milk first, then the other ingredients for a more even blend." ]
+```
+
+Unsure about if something is, in fact, a banana? Maybe it's in a language you don't understand!
+
+```javascript
+var unsureBanana = "바나나";
+banana.is(unsureBanana, "english");	// false
+banana.is(unsureBanana, "korean");	// true
+```
+
+If you're ever feeling insignificant, check to see if something is longer than the word banana in a specific language!
+
+```javascript
+var longer = "much larger";
+var shorter = "short";
+banana.isLongerThan(longer, "english");	 // false
+banana.isLongerThan(shorter, "english"); // true
+```
+
+Feeling down? Need some banana facts to pick up your spirits? Generate some random ones!
+
+```javascript
+banana.fact(); // 'Bananas are low in calories and have no fat, no sodium, and no cholesterol. They contain vitamin C, potassium, fiber, and vitamin B6.'
+banana.fact(); // 'Wrapping banana stems tightly in cling wrap will make them last three to five days longer.'
+banana.fact(); // 'Bananas are technically berries.'
+```
+
+**Other**
+```javascript
+banana.peel();		// 🍌
+banana.morse();		// -... .- -. .- -. .-
+banana.base64();	// YmFuYW5h
+banana.hex();		// #ffe135
+banana.color();		// yellow
+banana.phone();		// no.
+```
+
+## Development
+
+Like a real banana, all of the code is contained in one file:
+```
+./banana.js
+```
+
+Tests are in ``test.js`` and can be run using:
+```
+npm run test OR npm test
+```
+
+## License
+
+MIT
--- test.js
+++ test.js
@@ -1,43 +1,49 @@
 var assert = require('assert');
 var banana = require('./');
 
-assert.equal("Banana", banana(), "Should give Banana");
+assert.equal("🍌", 					banana.peel(), 				"Should be '🍌'");
+assert.equal("-... .- -. .- -. .-", banana.morse(), 			"Should be '-... .- -. .- -. .-'");
+assert.equal("YmFuYW5h",			banana.base64(), 			"Should be 'YmFuYW5h'");
+assert.equal("#ffe135",				banana.hex(),				"Should be '#ffe135'");
+assert.equal("yellow",				banana.color(),				"Should be 'yellow'");
+assert.equal("no.",					banana.phone(),				"Should be 'no.'");
 
-var isLongerThan = banana.isLongerThan("Dog");
-assert.equal(true, isLongerThan, "Dog has less characters than Banana, so it sould be false");
+assert.equal(true, 				  	banana.isLongerThan("Dog"), "Dog has less characters than Banana, so it sould be false");
 
-assert.equal("موز", banana.arabic(), "Banana should be موز in arabic");
-assert.equal("banan", banana.azerbaijani(), "Banana should be banan in azerbaijani");
-assert.equal("platano", banana.basque(), "Banana should be platano in basque");
-assert.equal("банан", banana.belarusian(), "Banana should be банан in belarusian");
-assert.equal("ငှက်ပျောသီး", banana.burmese(), "Banana should be ငှက်ပျောသီး in burmese");
-assert.equal("plàtan", banana.catalan(), "Banana should be plàtan in catalan");
-assert.equal("saging", banana.cebuano(), "Banana should be saging in cebuano");
-assert.equal("香蕉", banana.chinese(), "Banana should be 香蕉 in chinese");
-assert.equal("香蕉", banana.chinese("mandarin"), "Banana should be 香蕉 in mandarin");
-assert.equal("香蕉", banana.chinese("cantonese"), "Banana should be 香蕉 in cantonese");
-assert.equal("banán", banana.czech(), "Banana should be banán in czech");
-assert.equal("banan", banana.dutch(), "Banana should be banan in dutch");
-assert.equal("banano", banana.esperanto(), "Banana should be banano in esperanto");
-assert.equal("banana", banana.english(), "Banana should be banana in english");
-assert.equal("banaan", banana.estonian(), "Banana should be banaan in estonian");
-assert.equal("saging", banana.filipino(), "Banana should be saging in filipino");
-assert.equal("banaani", banana.finnish(), "Banana should be banaani in finnish");
-assert.equal("banane", banana.french(), "Banana should be banane in french");
-assert.equal("ბანანი", banana.georgian(), "Banana should be ბანანი in georgian");
-assert.equal("Banane", banana.german(), "Banana should be Banane in german");
-assert.equal("μπανάνα", banana.greek(), "Banana should be μπανάνα in greek");
-assert.equal("બનાના", banana.gujarati(), "Banana should be બનાના in gujarati");
-assert.equal("bannann", banana.haitianCreol(), "Banana should be bannann in haitian creol");
-assert.equal("ayaba", banana.hausa(), "Banana should be ayaba in hausa");
-assert.equal("בננה", banana.hebrew(), "Banana should be בננה in hebrew");
-assert.equal("केला", banana.hindi(), "Banana should be केला in hindi");
-assert.equal("banán", banana.hungarian(), "Banana should be banán in hungarian");
-assert.equal("banani", banana.icelandic(), "Banana should be banani in icelandic");
-assert.equal("unere", banana.igbo(), "Banana should be unere in igbo");
-assert.equal("pisang", banana.indonesian(), "Banana should be pisang in indonesian");
-assert.equal("バナナ", banana.japanese(), "Banana should be バナナ in japanese");
-assert.equal("ಬಾಳೆ", banana.kannada(), "Banana should be ಬಾಳೆ in kannada");
-assert.equal("банан", banana.kazakh(), "Banana should be банан in kazakh");
-assert.equal("ចេក", banana.khmer(), "Banana should be ចេក in khmer");
-assert.equal("바나나", banana.korean(), "Banana should be 바나나 in korean");
+assert.equal("زوم", 			  	banana.reversed("arabic"), 	"موز backwards should be زوم");
+assert.equal("းီသာေျပ်ကှင",	  	banana.reversed("burmese"), "ငှက်ပျောသီး backwards should be းီသာေျပ်ကှင");
+
+assert.equal("موز", 				banana("arabic"), 			"Should be 'موز'")
+assert.equal("banan", 				banana("azerbaijani"), 		"Should be 'banan'")
+assert.equal("platano", 			banana("basque"), 			"Should be 'platano'")
+assert.equal("банан", 				banana("belarusian"), 		"Should be 'банан'")
+assert.equal("ငှက်ပျောသီး", 	banana("burmese"), 			"Should be 'ငှက်ပျောသီး'")														
+assert.equal("plàtan", 				banana("catalan"), 			"Should be 'plàtan'")
+assert.equal("saging", 				banana("cebuano"), 			"Should be 'saging'")
+assert.equal("香蕉", 				banana("cantonese"), 		"Should be '香蕉'")
+assert.equal("banán", 				banana("czech"), 			"Should be 'banán'")
+assert.equal("banan", 				banana("dutch"), 			"Should be 'banan'")
+assert.equal("banano", 				banana("esperanto"), 		"Should be 'banano'")
+assert.equal("banana", 				banana("english"), 			"Should be 'banana'")
+assert.equal("banaan", 				banana("estonian"), 		"Should be 'banaan'")
+assert.equal("saging", 				banana("filipino"), 		"Should be 'saging'")
+assert.equal("banaani", 			banana("finnish"), 			"Should be 'banaani'")
+assert.equal("banane", 				banana("french"), 			"Should be 'banane'")
+assert.equal("ბანანი", 				banana("georgian"), 		"Should be 'ბანანი'")
+assert.equal("Banane", 				banana("german"), 			"Should be 'Banane'")
+assert.equal("μπανάνα", 			banana("greek"), 			"Should be 'μπανάνα'")
+assert.equal("બનાના", 				banana("gujarati"), 		"Should be 'બનાના'")
+assert.equal("bannann", 			banana("haitian creol"),	"Should be 'bannann'")
+assert.equal("ayaba", 				banana("hausa"),			"Should be 'ayaba'")
+assert.equal("בננה", 				banana("hebrew"),			"Should be 'בננה'")
+assert.equal("केला", 				banana("hindi"),			"Should be 'केला'")
+assert.equal("banán", 				banana("hungarian"),		"Should be 'banán'")
+assert.equal("banani", 				banana("icelandic"),		"Should be 'banani'")
+assert.equal("unere", 				banana("igbo"),				"Should be 'unere'")
+assert.equal("pisang", 				banana("indonesian"),		"Should be 'pisang'")
+assert.equal("バナナ", 				banana("japanese"),			"Should be 'バナナ'")
+assert.equal("ಬಾಳೆ", 				banana("kannada"),			"Should be 'ಬಾಳೆ'")
+assert.equal("банан", 				banana("kazakh"),			"Should be 'банан'")
+assert.equal("ចេក", 				banana("khmer"),			"Should be 'ចេក'")
+assert.equal("바나나", 				banana("korean"),			"Should be '바나나'")
+assert.equal("香蕉", 				banana("mandarin"),			"Should be '香蕉'")
